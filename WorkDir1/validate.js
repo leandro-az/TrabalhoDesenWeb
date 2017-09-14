82875c1b58e0f291aaa4b79170acb35a0c38a75d
lucasthimoteo <lucasthimoteo@id.uff.br>
2017-09-14 20:55:10 -0300

    verificaçao para todos os campos serem preenchidos

--- WorkDir1/validate.js
+++ WorkDir1/validate.js
@@ -1,13 +1,10 @@
     function validaCampos() {
-        var elemento = document.getElementById("nameTxt").value;
-	   if(elemento=="" || elemento==null){
-           alert("Preencha o campo: Nome Completo")
-           return false;
-       }
-        elemento = document.getElementById("cpfTxt").value;
-	   if(elemento=="" || elemento==null){
-           alert("Preencha o campo: CPF")
-           return false;
-       }
+        var elementos = document.getElementsByClassName("formInpt");
+        for(var i=0;i<elementos.length;i++){
+            if(elementos[i].value=="" || elementos[i].value==null){
+                alert("Preencha todos os campos.")
+                return false;
+            }
+        }
         return true;
 }
